1375db0712c9d1d7f1713ac732220c62248ec778
Gjc452 <1796444035@qq.com>
2021-02-09 21:58:45 +0800

    'node.js + AliveWeb'

---
+++ public/main.js
@@ -0,0 +1,317 @@
+function on(eventType, element, obj, fn) {
+  if (!(element instanceof Element)) {
+    element = document.querySelector(element);
+  }
+  element.addEventListener("click", (e) => {
+    if (obj instanceof Object) {
+      for (let key in obj) {
+        let t = e.target;
+        if (obj.hasOwnProperty(key)) {
+          while (!t.matches(key)) {
+            if (t === element) {
+              t = null;
+              break;
+            }
+            t = t.parentNode;
+          }
+          fn(t, key);
+        }
+      }
+    } else if (typeof obj === "string") {
+      let t = e.target;
+      try {
+        while (!t.matches(obj)) {
+          if (t === element) {
+            t = null;
+            break;
+          }
+          t = t.parentNode;
+        }
+        fn(t);
+      } catch (err) {}
+    }
+  });
+}
+
+const siteList = document.querySelector(".siteList");
+const addList = document.querySelector("#addList");
+const addContent = document.querySelector(".addContent");
+const reviseContent = document.querySelector(".reviseContent");
+let myHashMap = JSON.parse(window.localStorage.getItem("site"));
+let close;
+let site;
+let inIcon;
+const hashMap = myHashMap || [
+  {
+    url: "http://bonsaiden.github.io/JavaScript-Garden/zh/",
+    webName: "JavaScript 秘密花园",
+    disName: "JS秘密花园",
+    backColor: "red",
+  },
+  {
+    url: "https://wangdoc.com/",
+    webName: "网道 - 互联网开发文档",
+    disName: "网道",
+    backColor: "sky",
+  },
+  {
+    url: "https://developer.mozilla.org/zh-CN/docs/Web",
+    webName: "MDN",
+    disName: "Web开发技术 | MDN",
+    backColor: "blue",
+  },
+  {
+    url: "https://juejin.cn/",
+    webName: "掘金",
+    disName: "掘金",
+    backColor: "yellow",
+  },
+  {
+    url: "https://github.com/Gjc452",
+    webName: "我的GitHub",
+    disName: "GitHub",
+    backColor: "violet",
+  },
+  {
+    url: "https://www.yuque.com/xihang-c9xlp/guegsk",
+    webName: "我的博客",
+    disName: "语雀",
+    backColor: "blue",
+  },
+];
+
+// 渲染网站列表
+const render = () => {
+  let x = siteList.firstChild;
+  while (x) {
+    siteList.removeChild(x);
+    x = siteList.firstChild;
+  }
+  let id = 0;
+  hashMap.forEach((node, index) => {
+    const li = `
+      <li class= siteWrap${id} data="${id}">
+        <div class="site ${node.backColor}">
+          <div class="logo">
+            <text x="45" y="45" fill="white" text-anchor="middle" dominant-baseline="middle"
+            font-size="20px">
+              ${node.disName}
+            </text>
+            <svg class="icon  inIcon" aria-hidden="true">
+              <use xlink:href="#icon-revise"></use>
+            </svg>
+          </div>
+          <div class="close">
+            <svg class="icon" aria-hidden="true">
+              <use xlink:href="#icon-close"></use>
+            </svg>
+          </div>
+         </div>
+         <div class="link">${node.webName}</div>
+      </li>
+    `;
+    const add = document.createElement("template");
+    add.innerHTML = li.trim();
+    siteList.appendChild(add.content.firstChild);
+    let siteWrap = document.querySelector(".siteWrap" + id.toString());
+    on("click", siteWrap, ".close", (t) => {
+      if (t) {
+        hashMap.splice(index, 1);
+        render();
+      } else if (siteList.firstChild.classList.length === 2) {
+      } else {
+        window.open(node.url);
+      }
+    });
+    id += 1;
+  });
+  close = document.querySelectorAll(".close");
+  site = document.querySelectorAll(".site");
+  inIcon = document.querySelectorAll(".inIcon");
+};
+render();
+
+// 存储数据
+window.onbeforeunload = () => {
+  window.localStorage.setItem("site", JSON.stringify(hashMap));
+};
+
+// 鼠标右键删除样式
+siteList.addEventListener("mousedown", (e) => {
+  let t = e.target;
+  while (!t.matches("li")) {
+    if (t === siteList) {
+      t = null;
+      break;
+    }
+    t = t.parentNode;
+  }
+  if (t) {
+    if (e.button === 2) {
+      close.forEach((node) => {
+        node.classList.add("active");
+      });
+      site.forEach((node) => {
+        node.classList.add("active");
+      });
+      inIcon.forEach((node) => {
+        node.classList.add("active");
+      });
+      siteList.childNodes.forEach((node) => {
+        node.classList.add("deleteWeb");
+      });
+    }
+    siteList.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+    });
+  }
+});
+
+// 取消删除样式
+on("click", document.body, ".site", (t) => {
+  if (!t) {
+    close.forEach((node) => {
+      node.classList.remove("active");
+    });
+    site.forEach((node) => {
+      node.classList.remove("active");
+    });
+    inIcon.forEach((node) => {
+      node.classList.remove("active");
+    });
+    siteList.childNodes.forEach((node) => {
+      node.classList.remove("deleteWeb");
+    });
+  }
+});
+
+// 显示添加栏
+addList.addEventListener("click", () => {
+  setTimeout(() => {
+    addContent.classList.add("active");
+  });
+});
+siteList.addEventListener("click", (e) => {
+  let t = e.target;
+  while (!t.matches(".deleteWeb")) {
+    if (t === siteList || t.matches(".close")) {
+      t = null;
+      break;
+    }
+    t = t.parentNode;
+  }
+  if (t) {
+    setTimeout(() => {
+      reviseContent.classList.add("active");
+    });
+  }
+});
+// 隐藏添加栏
+const closeAddContent = document.querySelectorAll(".closeAddContent");
+on("click", document.body, ".addContent", (t) => {
+  if (addContent.classList.length === 2) {
+    if (!t) {
+      addContent.classList.remove("active");
+    }
+  }
+});
+on("click", document.body, ".reviseContent", (t) => {
+  if (reviseContent.classList.length === 2) {
+    if (!t) {
+      reviseContent.classList.remove("active");
+    }
+  }
+});
+closeAddContent.forEach((node) => {
+  node.addEventListener("click", () => {
+    reviseContent.classList.remove("active");
+    addContent.classList.remove("active");
+  });
+});
+
+// 添加网站
+const addWebUrl = document.querySelector("#addWebUrl");
+const addWebName = document.querySelector("#addWebName");
+const addDisName = document.querySelector("#addDisName");
+const refer = document.querySelector("#addRefer");
+const color = document.querySelector(".color");
+const colorList = document.querySelector(".colorList");
+let webRecord = { url: "", webName: "", disName: "", backColor: "" };
+let newUrl;
+let newWebName;
+let disName;
+let newBackColor = "red";
+addWebUrl.addEventListener("input", (e) => {
+  newUrl = e.target.value;
+});
+addWebName.addEventListener("input", (e) => {
+  newWebName = e.target.value;
+});
+addDisName.addEventListener("input", (e) => {
+  newDisName = e.target.value;
+});
+on("click", colorList, "li", (t) => {
+  let className = t.className;
+  color.className = `color ${className}`;
+  newBackColor = className;
+});
+refer.addEventListener("click", () => {
+  if (newUrl) {
+    webRecord.url = "https://" + newUrl;
+    newWebName
+      ? (webRecord.webName = newWebName.trim())
+      : (webRecord.webName = "");
+    newDisName
+      ? (webRecord.disName = newDisName.trim())
+      : (webRecord.disName = "");
+    webRecord.backColor = newBackColor;
+    hashMap.push(webRecord);
+    render();
+    addWebUrl.value = "";
+    addWebName.value = "";
+    addDisName.value = "";
+    color.className = "red";
+    webRecord = { url: "", webName: "", disName: "", backColor: "" };
+  } else {
+    alert("请填写网址");
+  }
+});
+// 修改网站
+const nowWebName = document.querySelector("#webName");
+const nowDisName = document.querySelector("#disName");
+const nowUrl = document.querySelector("#webUrl");
+const nowColor = document.querySelector(".nowColor");
+const nowColorList = document.querySelector(".nowColorList");
+const change = document.querySelector("#change");
+let m;
+let nowBackColor;
+siteList.addEventListener("click", (e) => {
+  let t = e.target;
+  while (!t.matches("li")) {
+    if (t === siteList || t.matches(".close")) {
+      t = null;
+      break;
+    }
+    t = t.parentNode;
+  }
+  if (t) {
+    m = t.getAttribute("data");
+    nowUrl.value = hashMap[m].url;
+    nowDisName.value = hashMap[m].disName;
+    nowWebName.value = hashMap[m].webName;
+    let className = hashMap[m].backColor;
+    nowColor.className = `color ${className}`;
+  }
+});
+on("click", nowColorList, "li", (t) => {
+  let className = t.className;
+  nowColor.className = `color ${className}`;
+  nowBackColor = className;
+});
+change.addEventListener("click", () => {
+  hashMap[m].url = nowUrl.value;
+  hashMap[m].disName = nowDisName.value;
+  hashMap[m].webName = nowWebName.value;
+  hashMap[m].backColor = nowBackColor;
+  render();
+});
---
+++ public/search.js
@@ -0,0 +1,187 @@
+const google = {
+  ".kindWeb": "https://www.google.com/search?q=",
+  ".kindImg": "https://www.google.com/search?tbm=isch&q=",
+  ".kindNews": "https://www.google.com/search?tbm=nws&q=",
+  ".kindAvi": "https://www.google.com/search?tbm=vid&q=",
+  ".kindMap": "https://www.google.com/maps/preview?q=",
+};
+const bing = {
+  ".kindWeb":
+    "https://www.bing.com/search?isource=infinity&iname=bing&itype=web&q=",
+  ".kindImg":
+    "https://cn.bing.com/images/search?isource=infinity&iname=bing&q=",
+  ".kindNews":
+    "https://www.bing.com/news/search?isource=infinity&iname=bing&q=",
+  ".kindAvi":
+    "https://cn.bing.com/videos/search?isource=infinity&iname=bing&q=",
+  ".kindMap": "https://www.bing.com/maps/?isource=infinity&iname=bing&q=",
+};
+const baidu = {
+  ".kindWeb": "https://www.baidu.com/s?tn=02003390_42_hao_pg&ie=utf-8&wd=",
+  ".kindImg":
+    "https://image.baidu.com/search/index?tn=baiduimage&ie=utf-8&word=",
+  ".kindNews":
+    "https://www.baidu.com/s?rtt=1&bsst=1&cl=2&tn=news&rsv_dl=ns_pc&word=",
+  ".kindAvi": "http://video.baidu.com/v?ie=utf-8&word=",
+  ".kindMap": "https://map.baidu.com/?newmap=1&ie=utf-8&s=s%26wd%3D",
+};
+const sogou = {
+  ".kindWeb":
+    "https://www.sogou.com/sogou?pid=sogou-site-7985672db979303a&query=",
+  ".kindImg": "https://pic.sogou.com/pics?ie=utf8&query=",
+  ".kindNews":
+    "https://www.sogou.com/sogou?ie=utf8&interation=1728053249&interV=&pid=sogou-wsse-9fc36fa768a74fa9&ie=utf8&query=",
+  ".kindAvi": "https://v.sogou.com/v?ie=utf8&query=",
+};
+function on(eventType, element, obj, fn) {
+  if (!(element instanceof Element)) {
+    element = document.querySelector(element);
+  }
+  element.addEventListener("click", (e) => {
+    if (obj instanceof Object) {
+      for (let key in obj) {
+        let t = e.target;
+        if (obj.hasOwnProperty(key)) {
+          while (!t.matches(key)) {
+            if (t === element) {
+              t = null;
+              break;
+            }
+            t = t.parentNode;
+          }
+          fn(t, key);
+        }
+      }
+    } else if (typeof obj === "string") {
+      let t = e.target;
+      try {
+        while (!t.matches(obj)) {
+          if (t === element) {
+            t = null;
+            break;
+          }
+          t = t.parentNode;
+        }
+        fn(t);
+      } catch (err) {}
+    }
+  });
+}
+
+const changeImg = document.querySelector(".changeImg");
+const searchClass = document.querySelector(".searchClass");
+const searchUl = document.querySelector(".searchUl");
+const others = document.querySelector(".others");
+const search = document.querySelector("#search");
+const changeSearch = document.querySelector(".changeSearch");
+let string;
+const objWeb = {
+  baidu,
+  google,
+  bing,
+  sogou,
+};
+let names = objWeb.baidu;
+let url = "https://www.baidu.com/s?tn=02003390_42_hao_pg&ie=utf-8&wd=";
+let angle = 20;
+let n = 0;
+
+//更换壁纸
+changeImg.addEventListener("click", () => {
+  n += 1;
+  angle += 20;
+  changeImg.style.transform = `rotate(${angle}deg)`;
+  document.body.style.background = `no-repeat url(https://bing.ioliu.cn/v1/rand?t=${n})`;
+});
+
+//  阻止默认事件
+changeSearch.addEventListener("click", (e) => {
+  e.preventDefault();
+});
+
+// 开关搜索选择框
+on("click", document.body, ".changeSearch", (t) => {
+  if (t) {
+    if (others.classList.length === 1) {
+      others.classList.add("active");
+    } else {
+      others.classList.remove("active");
+    }
+  } else {
+    others.classList.remove("active");
+  }
+});
+
+// 切换搜索引擎
+on("click", others, "li", (t) => {
+  for (let key in objWeb) {
+    if (key === t.className) {
+      const svgName = t.className;
+      const svgNode = `
+        <svg class="icon" aria-hidden="true">
+          <use xlink:href="#icon-${svgName}"></use>
+        </svg>
+        <svg class="icon" aria-hidden="true">
+          <use xlink:href="#icon-f111"></use>
+        </svg>`;
+      const li = `
+        <li class="kindMap">地图</li>`;
+      const template = document.createElement("template");
+      template.innerHTML = svgNode.trim();
+      changeSearch.innerHTML = "";
+      changeSearch.appendChild(template.content);
+      names = objWeb[key];
+      const list = searchUl.childNodes;
+      const kindMap = document.querySelector(".kindMap");
+      if (key === "sogou") {
+        const kindMap = document.querySelector(".kindMap");
+        searchUl.removeChild(kindMap);
+      } else if (kindMap) {
+      } else {
+        const template = document.createElement("template");
+        template.innerHTML = li.trim();
+        searchUl.appendChild(template.content.firstChild);
+      }
+      for (let i = 0; i < list.length; i++) {
+        if (list[i].nodeType === 1) {
+          if (list[i].classList[0] === "kindWeb") {
+            list[i].classList.add("active");
+          } else {
+            list[i].classList.remove("active");
+          }
+        }
+      }
+      for (let key in names) {
+        key === ".kindWeb" ? (url = names[key]) : "";
+      }
+      return;
+    }
+  }
+});
+
+// 切换搜索类
+on("click", searchClass, names, (t, key) => {
+  if (t) {
+    url = names[key];
+    t.classList.add("active");
+  } else {
+    let node = document.querySelector(`${key}`);
+    node.classList.remove("active");
+  }
+});
+
+// 获取输入内容
+search.addEventListener("input", (e) => {
+  string = e.target.value;
+});
+
+// 按下搜索
+search.addEventListener("keydown", (e) => {
+  if (e.code === "Enter") {
+    if (string) {
+      window.open(`${url}${string}`);
+    } else {
+      window.open(`${url}`);
+    }
+  }
+});
